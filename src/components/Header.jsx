ad8abf033037fe04c80d3f6b4cd286760e62c1eb
tolanny-studio <hellotolannystudio@gmail.com>
2026-02-27 00:43:05 +0100

    Add get location functionality to form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,11 @@ import { CiSearch } from "react-icons/ci";
 import { SiAccuweather } from "react-icons/si";
 
 const Header = () => {
+  const handleSearchSubmit = (FormData) =>{
+    const location = FormData.get('location')
+    console.log(location);
+    
+  }
   return (
     <div className="flex flex-col justify-center items-center bg-blue-50 h-auto py-2 gap-1.5 shadow-2xs ">
       <div className="flex justify-center items-center text-3xl">
@@ -9,11 +14,11 @@ const Header = () => {
           <SiAccuweather />
         </span>
       </div>
-      <form className="">
+      <form action={handleSearchSubmit} className="">
         <div className="flex justify-center items-center bg-blue-300 rounded-full shadow-md">
           <input
             className="rounded-tl-full rounded-bl-full focus:outline-0 p-0.5 pl-4 text-white text-xs"
-            type="text"
+            type="text" name="location"
           />
           <span className="text-2xl text-blue-500 p-0.5 bg-white rounded-tr-full rounded-br-full">
             <CiSearch />
